8ad41617d07e1ee937e84410919b8c66493831fc
Sabrina Silva <76228258+Sabrina51@users.noreply.github.com>
2024-05-22 15:34:08 -0300

    add request lib

--- src/model/index.js
+++ src/model/index.js
@@ -1,13 +1,37 @@
-async function getInformation(dbConfig) {
+// var request = require("request");
+import request from "request";
 
-  const result = dbConfig.postAllDocs({
-    db: 'healthy-weight-db',
-    includeDocs: true
-  }).then(response => {
-    return response.result.rows
+async function getInformation(dbConfig) {
+  var options = {
+    method: "POST",
+    url: "https://oauth.fatsecret.com/connect/token",
+    auth: {
+      user: "",
+      password: "",
+    },
+    headers: { "content-type": "application/x-www-form-urlencoded" },
+    form: {
+      grant_type: "client_credentials",
+      // scope: "basic",
+      scope: "premier",
+    },
+    json: true,
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+    return body;
   });
-  return result
+
+  // const result = dbConfig.postAllDocs({
+  //   db: 'healthy-weight-db',
+  //   includeDocs: true
+  // }).then(response => {
+  //   return response.result.rows
+  // });
+  // return result
 }
 
-
-export default { getInformation }
+export default { getInformation };
