7cd22cb7d15625a4974ac2469b715648933bda49
Sabrina Silva <76228258+Sabrina51@users.noreply.github.com>
2025-03-24 21:59:58 -0300

    sonarSever test

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,6 +2,15 @@ import dbConfig from "../database/index.js";
 import model from "../model/index.js";
 
 async function foods(req, res) {
+  const senhaFatec = "B4GW784kn12lpaPtPkL02F_KPV6F";
+  const testFatec;
+
+      function sum(a, b) {
+        return a + b;
+      }
+  
+      sum(1, 2, 3);
+
   let db = await dbConfig();
   let response = await model.getFoods(db);
   res.status(200).json(response);
