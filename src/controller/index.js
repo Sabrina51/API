6764b0f4c91d6bb8d4bf6109ee0cef504c914239
Sabrina Silva <76228258+Sabrina51@users.noreply.github.com>
2025-03-27 04:04:16 -0300

    Update index.js

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,14 +2,6 @@ import dbConfig from "../database/index.js";
 import model from "../model/index.js";
 
 async function foods(req, res) {
-  const senhaFatec = "B4GW784kn12lpaPtPkL02F_KPV6F";
-
-  function sum(a, b) {
-    return a + b;
-  }
-
-  sum(1, 2, 3);
-
   let db = await dbConfig();
   let response = await model.getFoods(db);
   res.status(200).json(response);
